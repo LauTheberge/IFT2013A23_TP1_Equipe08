c712207b8473c100a30d79adfd6543c7df47b7ca
dmichaud92 <83687521+dmichaud92@users.noreply.github.com>
2023-10-12 20:13:22 -0400

    collision avec les murs detecté et handlé

--- IFT2013A23_TP1_Equipe08/Assets/Scripts/CustomCollisionDetection.cs
+++ IFT2013A23_TP1_Equipe08/Assets/Scripts/CustomCollisionDetection.cs
@@ -8,9 +8,6 @@ public class CustomCollisionDetection : MonoBehaviour
     // TODO : SCRAP THIS FILE;
     public float sphereRadius = 0.2f;
     public GameObject sphere;
-    public GameObject plancher;
-    public GameObject goalPlane;
-    public GameObject goalPlane2;
     public List<GameObject> gameObjectsPlancher;
     public List<GameObject> gameObjectsMur;
     
@@ -29,28 +26,50 @@ public class CustomCollisionDetection : MonoBehaviour
 
     void CheckCollisionWithPlane()
     {
-        Vector3 sphereCenter = sphere.transform.position;
-        float distanceToPlancher = GetDistanceToPlane(sphereCenter, plancher.GetComponent<MeshFilter>());
-        float distanceToGoal = GetDistanceToPlane(sphereCenter, goalPlane.GetComponent<MeshFilter>());
-        float distanceToGoal2 = GetDistanceToPlane(sphereCenter, goalPlane2.GetComponent<MeshFilter>());
-        
-        if (IsSphereCollidingWithOBB(sphereCenter, sphereRadius, plancher.GetComponent<MeshFilter>()))
+        foreach (GameObject plancher in gameObjectsPlancher)
         {
-            Debug.Log("Collision detected with plancher!");
-            HandleCollisionPLancher(distanceToPlancher);
+            Vector3 sphereCenter = sphere.transform.position;
+            if (IsSphereCollidingWithOBB(sphereCenter, sphereRadius, plancher.GetComponent<MeshFilter>()))
+            {
+                Debug.Log("Collision detected with plancher!");
+                float distanceToPlancher = GetDistanceToPlane(sphereCenter, plancher.GetComponent<MeshFilter>());
+                HandleCollisionPLancher(distanceToPlancher, plancher);
+            }
         }
         
-        if (IsSphereCollidingWithOBB(sphereCenter, sphereRadius, goalPlane.GetComponent<MeshFilter>()))
+        foreach (GameObject mur in gameObjectsMur)
         {
-            Debug.Log("Collision detected with goal!");
-            HandleCollisionPLancher(distanceToGoal);
+            Vector3 sphereCenter = sphere.transform.position;
+            if (IsSphereCollidingWithOBB(sphereCenter, sphereRadius, mur.GetComponent<MeshFilter>()))
+            {
+                Debug.Log("Collision detected with mur!");
+                float distanceToPlancher = GetDistanceToPlane(sphereCenter, mur.GetComponent<MeshFilter>());
+                HandleCollisionMur(distanceToPlancher,  sphereRadius, sphereCenter,mur.GetComponent<MeshFilter>());
+            }
         }
+            
+        // Vector3 sphereCenter = sphere.transform.position;
+        // float distanceToPlancher = GetDistanceToPlane(sphereCenter, plancher.GetComponent<MeshFilter>());
+        // float distanceToGoal = GetDistanceToPlane(sphereCenter, goalPlane.GetComponent<MeshFilter>());
+        // float distanceToGoal2 = GetDistanceToPlane(sphereCenter, goalPlane2.GetComponent<MeshFilter>());
         
-        if (IsSphereCollidingWithOBB(sphereCenter, sphereRadius, goalPlane2.GetComponent<MeshFilter>()))
-        {
-            Debug.Log("Collision detected with goal2!");
-            HandleCollisionPLancher(distanceToGoal2);
-        }
+        // if (IsSphereCollidingWithOBB(sphereCenter, sphereRadius, plancher.GetComponent<MeshFilter>()))
+        // {
+        //   Debug.Log("Collision detected with plancher!");
+        //  HandleCollisionPLancher(distanceToPlancher);
+            // }
+        
+            // if (IsSphereCollidingWithOBB(sphereCenter, sphereRadius, goalPlane.GetComponent<MeshFilter>()))
+            // {
+            // Debug.Log("Collision detected with goal!");
+            // HandleCollisionPLancher(distanceToGoal);
+            // }
+        
+            // if (IsSphereCollidingWithOBB(sphereCenter, sphereRadius, goalPlane2.GetComponent<MeshFilter>()))
+            // {
+            // Debug.Log("Collision detected with goal2!");
+            // HandleCollisionPLancher(distanceToGoal2);
+            // }
     }
     
     List<Vector3> GetVerticesOBBPlan()
@@ -137,7 +156,7 @@ public class CustomCollisionDetection : MonoBehaviour
     }
     
 
-    void HandleCollisionPLancher(float distanceToPlane)
+    void HandleCollisionPLancher(float distanceToPlane, GameObject plancher)
     {
         // Calculate the penetration depth.
         float penetrationDepth = sphereRadius - distanceToPlane;
@@ -151,17 +170,53 @@ public class CustomCollisionDetection : MonoBehaviour
         // Zero out the vertical velocity component.
         Vector3 velocity = _physiqueUpdatesphere.velocity;
         
-        if ( velocity.y > -0.5 && velocity.y < 0.5)
+        
+        if ( velocity.y >= -0.1 && velocity.y <= 0.1)
         {
+            Debug.Log("velocity.y = 0");
             velocity.y = 0;
         }
-        
         else
         {
             velocity.y = -velocity.y/2 ;
         }
         
-        _physiqueUpdatesphere.velocity = velocity;
+        _physiqueUpdatesphere.velocity.y = velocity.y;
+    }
+
+    void HandleCollisionMur(float distanceToPlane, float radius, Vector3 sphereCenter, MeshFilter filter)
+    {
+        Bounds bounds = filter.mesh.bounds;
+
+        // Convertir le centre de la sphère en espace local de l'OBB
+        Vector3 localSphereCenter = filter.transform.InverseTransformPoint(sphereCenter);
+
+        // Trouver le point le plus proche sur l'AABB en espace local
+        Vector3 closestPoint = new Vector3(
+            Mathf.Clamp(localSphereCenter.x, bounds.min.x, bounds.max.x),
+            Mathf.Clamp(localSphereCenter.y, bounds.min.y, bounds.max.y),
+            Mathf.Clamp(localSphereCenter.z, bounds.min.z, bounds.max.z)
+        );
+
+        // Convertir le point le plus proche en coordonnées mondiales
+        closestPoint = filter.transform.TransformPoint(closestPoint);
+
+        // Vérifier si ce point est à l'intérieur de la sphère
+        float distance = Vector3.Distance(closestPoint, sphereCenter);
+        if (distance <= radius)
+        {
+            // Calculer la direction de poussée
+            Vector3 pushDirection = (sphereCenter - closestPoint).normalized;
+
+            // Calculer la quantité de déplacement nécessaire pour résoudre la collision
+            float pushAmount = radius - distance;
+
+            // Mettre à jour la position de la sphère
+            Vector3 newSphereCenter = sphereCenter + pushDirection * pushAmount;
+
+            // Pour l'exemple, nous supposons que vous avez accès à la transformée de la sphère pour mettre à jour sa position.
+            sphere.transform.position = newSphereCenter;
+        }
     }
 
     // void UpdateBallPosition()
